922d4be92148c39778f2772829dab62ac3d89392
JessicaLima <jessikurt@gmail.com>
2021-11-01 16:14:57 -0300

    feat: add Home on Pages

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: #eee;
+  width: 768px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
