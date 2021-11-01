f7f0ded27f7e2c95e01b7b9c45e81c235a71a98a
JessicaLima <jessikurt@gmail.com>
2021-11-01 15:53:32 -0300

    add storybook

---
+++ src/stories/CharapterCard.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { CharapterCard } from '../components/CharapterCard';
+
+export default {
+  title: 'Component/CharapterCard',
+  component: CharapterCard,
+};
+
+export const Component = () => <CharapterCard />;
---
+++ src/stories/CharapterList.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { CharapterList } from '../components/CharapterList';
+
+export default {
+  title: 'Component/CharapterList',
+  component: CharapterList,
+};
+
+export const Component = () => <CharapterList />;
+
