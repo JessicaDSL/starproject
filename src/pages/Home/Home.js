f405fe6b25fd8ddd689dc9788c2ccb3dd924f192
JessicaLima <jessikurt@gmail.com>
2021-11-03 15:10:26 -0300

    feat: add axios

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-import { Container } from './styles'
-import CharapterList from '../../components/CharapterList'
+import { Container } from "./styles";
+import CharacterList from "../../components/CharacterList";
 
 const Home = () => {
+  const [character, setCharacter] = useState([]);
+
+  useEffect(() => {
+    axios(`https://swapi.dev/api/people/`)
+      .then((response) => {
+        const person = response.data?.results?.map((item) => {
+          const name = item?.name;
+          const eyeColor = item?.eye_color;
+          return { name, eyeColor };
+        });
+        setCharacter(person);
+      })
+      .catch((error) => {
+        console.log("Error: ", error);
+      });
+  }, []);
+
+  function handleDelete(name) {
+    character.map(() => {
+      const characterRemoved = character.filter(
+        (person) => person.name !== name
+      );
+      return setCharacter(characterRemoved);
+    });
+  }
 
   return (
     <Container>
-      <CharapterList />
+      <h1>Personagens StarWars</h1>
+      <CharacterList character={character} handleDelete={handleDelete} />
     </Container>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
